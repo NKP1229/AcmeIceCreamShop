dac509204235e1a4fefb87cd99dc48298d5139c2
NKP1229 <113551880+NKP1229@users.noreply.github.com>
2025-04-17 22:00:12 -0400

    displays all flavors in flavor table in main.jsx file (GET /api/flavors)

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from "react-dom/client";
 import axios from "axios";
 
 const App = () => {
-  const [employees, setEmployees] = useState([]);
+  const [flavors, setFlavors] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchEmployees = async () => {
-      const response = await axios.get("/");
+      const response = await axios.get("/api/flavors");
+      setFlavors(response.data);
+      setIsLoading(false);
     };
     fetchEmployees();
   }, []);
@@ -19,7 +21,20 @@ const App = () => {
 
   return (
     <main>
-      <h1>Acme HR ({employees.length})</h1>
+      <h1>Acme Ice Cream Shop:</h1>
+      <h3>Total flavors: ({flavors.length})</h3>
+      <ul>
+        {flavors.map((flavor) => (
+          <li key={flavor.id}>
+            {flavor.name}
+            {flavor.is_favorite ? (
+              <span style={{ paddingLeft: "1rem", color: "red" }}>
+                (FAVORITE)
+              </span>
+            ) : null}
+          </li>
+        ))}
+      </ul>
     </main>
   );
 };
--- server/index.js
+++ server/index.js
@@ -27,7 +27,58 @@ app.get("/api/flavors", async (req, res, next) => {
     next(error);
   }
 });
-
+app.get("/api/flavors/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const SQL = `
+        SELECT FROM flavors WHERE id = $1      
+    `;
+    const response = await client.query(SQL, [id]);
+    res.status(200).json(response.rows);
+  } catch (error) {
+    next(error);
+  }
+});
+app.post("/api/flavors", async (req, res, next) => {
+  try {
+    const { name, is_favorite } = req.body;
+    const SQL = `
+            INSERT INTO users(name, is_favorite) VALUES($1, $2) RETURNING *
+        `;
+    const response = await client.query(SQL, [name, is_favorite]);
+    res.status(201).json(response.rows);
+  } catch (error) {
+    next(error);
+  }
+});
+app.delete("/api/flavors/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const SQL = `
+          DELETE FROM users WHERE id = $1
+      `;
+    await client.query(SQL, [id]);
+    res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
+});
+app.put("/api/flavors/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { is_favorite } = req.body;
+    const SQL = `
+              UPDATE users
+              SET is_favorite = $1 
+              where id = $2
+              RETURNING *
+          `;
+    const response = await client.query(SQL, [is_favorite, id]);
+    res.status(200).json(response.rows[0]);
+  } catch (error) {
+    next(error);
+  }
+});
 // create your init function
 const init = async () => {
   await client.connect();
